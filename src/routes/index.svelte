7ebfbd32820231577c04b9e32ccad929987b74b8
zhammer <zachary_hammer@alumni.brown.edu>
2020-09-28 19:45:16 -0400

    copy over sorting from apache index directory

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,60 @@
 <script context="module" lang="ts">
-  export async function preload() {
+  // query params from apache index directory
+  type Sort = {
+    column:
+      | "N" // name
+      | "M" // modified
+      | "S" // size
+      | "D"; // description
+    // order
+    order:
+      | "A" // ascending
+      | "D"; // descending
+  };
+  export async function preload({ query }) {
     let resp = await this.fetch("pieces.json");
-    let { pieces } = await resp.json();
-    return { pieces };
+    let { pieces }: { pieces: Piece[] } = await resp.json();
+    let sort: Sort = {
+      column: query.C || "N",
+      order: query.O || "A",
+    };
+    pieces.sort((a, b) => {
+      let [aField, bField] = [a, b].map<string>((piece) => {
+        switch (sort.column) {
+          case "N":
+            return piece.title;
+          case "M":
+            return piece.date;
+          case "S":
+            return piece.size.toString();
+          case "D":
+            return piece.description;
+        }
+      });
+
+      let comp = aField.localeCompare(bField);
+      if (sort.order === "D") {
+        comp = -comp;
+      }
+      return comp;
+    });
+    return { pieces, sort };
   }
 </script>
 
 <script lang="ts">
   import type { Piece } from "./pieces/_pieces";
   export let pieces: Piece[];
+
+  export let sort: Sort;
+
+  function orderParam(sort: Sort, column: Sort["column"]): Sort["order"] {
+    if (sort.column !== column) {
+      return "A";
+    }
+
+    return sort.order === "A" ? "D" : "A";
+  }
 
   // https://stackoverflow.com/a/9461657
   function format(num: number) {
@@ -25,20 +71,20 @@
 </style>
 
 <svelte:head>
-  <title>Index of Writing/</title>
+  <title>Index of /</title>
 </svelte:head>
 
 <div class="container">
-  <h1>Index of Writing/</h1>
+  <h1>Index of /</h1>
 
   <table>
     <tbody>
       <tr>
         <th valign="top" />
-        <th><a href="">Name</a></th>
-        <th><a href="">Last modified</a></th>
-        <th><a href="">Size</a></th>
-        <th><a href="">Description</a></th>
+        <th><a href="?C=N&O={orderParam(sort, 'N')}">Name</a></th>
+        <th><a href="?C=M&O={orderParam(sort, 'M')}">Last modified</a></th>
+        <th><a href="?C=S&O={orderParam(sort, 'S')}">Size</a></th>
+        <th><a href="?C=D&O={orderParam(sort, 'D')}">Description</a></th>
       </tr>
       <tr>
         <th colspan="5">
