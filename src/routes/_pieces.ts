58db53beef849f06829be6ad9827cebb46569f08
zhammer <zachary_hammer@alumni.brown.edu>
2020-12-23 08:43:29 -0500

    make readPiece its own function

--- src/routes/_pieces.ts
+++ src/routes/_pieces.ts
@@ -39,25 +39,25 @@ export type Piece = PieceListItem & {
   scenes: Scene[];
 };
 
+function readPiece(filename: string): Piece {
+  let file = fs.readFileSync("pieces/" + filename);
+  let body = file.toString();
+  let piece: Piece = yml.parse(body);
+  return {
+    ...piece,
+    scenes: piece.scenes.map((scene) => ({
+      ...scene,
+      type: scene.type || "Default",
+    })),
+    size: body.length,
+  };
+}
+
 export function loadPieces(): Piece[] {
   let filenames = fs.readdirSync("pieces", { withFileTypes: true });
   return filenames
     .filter((filename) => filename.isFile())
-    .map(
-      (filename): Piece => {
-        let file = fs.readFileSync("pieces/" + filename.name);
-        let body = file.toString();
-        let piece: Piece = yml.parse(body);
-        return {
-          ...piece,
-          scenes: piece.scenes.map((scene) => ({
-            ...scene,
-            type: scene.type || "Default",
-          })),
-          size: body.length,
-        };
-      }
-    );
+    .map((filename): Piece => readPiece(filename.name));
 }
 
 export function loadPieceListItems(): PieceListItem[] {
