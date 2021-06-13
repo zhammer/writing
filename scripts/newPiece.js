5e39d918f02f129adcfd399b9a4c0b87bff2ac9c
Zach Hammer <zachary_hammer@alumni.brown.edu>
2021-06-13 19:35:15 -0400

    add newPiece function

---
+++ scripts/newPiece.js
@@ -0,0 +1,32 @@
+const { writeFileSync } = require("fs");
+const { argv } = require("process");
+
+function slugify(text) {
+    return text.toString().toLowerCase()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+        .replace(/\-\-+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, ''); // Trim - from end of text
+}
+
+let title = argv[2];
+let slug = slugify(title);
+let today = new Date().toISOString().split('T')[0];
+
+let piece = `\`\`\`piece
+title: ${title}
+slug: ${slug}
+date: ${today}
+description: ""
+
+# meta:
+#   song:
+#     name: 
+#     artist:
+#     url:
+\`\`\`
+`;
+
+
+writeFileSync(`${slug}.txt`, piece);
