9af8713e31870bfd50ed5b46acb96bb1c551ad3c
Zach Hammer <zachary_hammer@alumni.brown.edu>
2021-06-14 09:52:16 -0400

    switch plugins/index.js to ts

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -5,5 +5,5 @@ import cucumber from "cypress-cucumber-preprocessor";
  * @type {Cypress.PluginConfig}
  */
 export default (on, config) => {
-    on("file:preprocessor", cucumber());
+    on("file:preprocessor", cucumber.default());
 };
