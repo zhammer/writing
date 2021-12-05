20ae26d295d62b1f9cff5ebdffba7cd5cfcb588e
Zach Hammer <zachary_hammer@alumni.brown.edu>
2021-12-05 16:50:42 -0500

    add support for bookplaylist type pieces

---
+++ scripts/hydrateBookplaylist.js
@@ -0,0 +1,32 @@
+import { writeFile, readFile } from "fs/promises";
+import fetch from "node-fetch";
+import { argv } from "process";
+import yml from "yaml";
+
+function pluckBook(payload) {
+    let firstBook = payload.items[0];
+    let volumeInfo = firstBook.volumeInfo;
+    return {
+        title: volumeInfo.title,
+        author: volumeInfo.authors[0],
+        image: volumeInfo.imageLinks.thumbnail,
+    }
+}
+
+let filePath = argv[2];
+
+let file = await readFile(filePath);
+let bookplaylist = yml.parse(file.toString());
+if (!bookplaylist.isbn) {
+    console.log("no isbn, exiting")
+    process.exit(0);
+}
+let res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${bookplaylist.isbn}`);
+let resJson = await res.json();
+let book = pluckBook(resJson);
+let hydrated = {
+    ...bookplaylist,
+    book,
+}
+let out = yml.stringify(hydrated);
+await writeFile(filePath, out);
---
+++ src/plugins/bookplaylist.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import type { ProcessedBookPlaylist } from "../routes/_pieces";
+
+  export let piece: ProcessedBookPlaylist;
+</script>
+
+<div class="body">
+  <div class="page">
+    <div>
+      <h1>{piece.book.title} <i>by {piece.book.author}</i></h1>
+      <img src={piece.book.image} alt="book cover" />
+      <p>{piece.note}</p>
+      <iframe
+        title="playlist"
+        src={piece.playlist.replace(".com/", ".com/embed/")}
+        width="300"
+        height="380"
+        frameborder="0"
+        allowtransparency={true}
+        allow="encrypted-media"
+      />
+    </div>
+  </div>
+</div>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  .body {
+    background-image: url("/candle.gif");
+    font-family: helvetica, arial, sans-serif;
+    font-size: 16px;
+    height: 110vh;
+  }
+
+  .page {
+    padding-top: 1em;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+
+  h1,
+  p {
+    color: lightyellow;
+    background-color: black;
+  }
+
+  p {
+    white-space: pre-wrap;
+  }
+</style>
