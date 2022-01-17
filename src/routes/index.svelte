b6444728b872dd36d9183eefb42c7b2c674747d1
Zach Hammer <zhammer@seatgeek.com>
2022-01-17 12:24:30 -0500

    bump deps

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script context="module" lang="ts">
-  export async function load({ page, fetch }) {
+  export async function load({ url, fetch }) {
     let resp = await fetch("pieces.json");
     let data = await resp.json();
     let query = new URLSearchParams();
     try {
-      query = page.query;
+      query = url.searchParams;
     } catch (e) {}
     return { props: { directory: data.directory, query: query } };
   }
