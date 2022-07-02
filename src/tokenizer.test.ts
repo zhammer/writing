5059eeadfb02e029c0a671b674261f1c2756a652
Zach Hammer <zhammer@seatgeek.com>
2022-07-02 16:39:15 -0400

    i fucking hate javascript

--- src/tokenizer.test.ts
+++ src/tokenizer.test.ts
@@ -1,18 +1,21 @@
-import { tokenizer } from './tokenizer';
+import { tokenizer, type Token } from './tokenizer';
 
 describe('tokenizer', () => {
   // success cases
   test.each([
-    ["no brackets", "this text doesn't have brackets", ["this text doesn't have brackets"]],
-    ["brackets with space", "this text has brackets{{ footnote \"they are great brackets!\" }}", ["this text has brackets", "footnote \"they are great brackets!\""]]
+    ["no brackets", "this text doesn't have brackets", [{ type: "literal", text: "this text doesn't have brackets" }]],
+    [
+      "brackets with space", `this text has brackets{{ footnote "they are great brackets!" }}`,
+      [{ type: "literal", text: "this text has brackets" }, { type: "function", functionName: "footnote", args: ["they are great brackets!"] }]
+    ]
   ])
-    ('%s', (_, text: string, expected: string[]) => {
+    ('%s', (_, text: string, expected: Token[]) => {
       expect(tokenizer(text)).toEqual(expected);
     })
 
   // error cases
   test.each([
-    ["unended bracket", "this text {{ has brackets but they don't close", "finished parsing without closing brackets"]
+    ["unended bracket", `this text {{ has brackets but they don't close`, "finished parsing without closing brackets"]
   ])
     ('%s', (_, text: string, expected: string) => {
       expect(() => { tokenizer(text) }).toThrow(expected);
--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,22 +1,44 @@
-export function tokenizer(text: string): string[] {
-  let out: string[] = [];
+export type TokenLiteral = {
+  type: "literal";
+  text: string;
+}
+
+export type TokenFunction = {
+  type: "function";
+  functionName: string;
+  args: string[];
+}
+
+export type Token = TokenLiteral | TokenFunction;
+
+export function tokenizer(text: string): Token[] {
+  let out: Token[] = [];
   let state: "idle" | "in_bracket" = "idle";
   let curr = text;
   while (!!curr) {
     let before: string, after: string;
     switch (state) {
       case "idle":
-        [before, after] = curr.split(/{{\s*/, 2);
-        out.push(before);
+        [before, after] = splitOnce(curr, "{{")
+        out.push({
+          type: "literal",
+          text: before,
+        });
         curr = after;
         state = "in_bracket";
         break;
       case "in_bracket":
-        [before, after] = curr.split(/\s*}}/, 2);
+        [before, after] = splitOnce(curr, "}}");
         if (after === undefined) {
           throw new Error("finished parsing without closing brackets");
         }
-        out.push(before);
+        let [functionName, argText] = splitOnce(before.trim(), " ");
+        let args = [...(argText || "").matchAll(/\"([^\"]*)\"/g)].map(match => match[1])
+        out.push({
+          type: "function",
+          functionName: functionName.trim(),
+          args,
+        });
         curr = after;
         state = "idle";
         break;
@@ -24,4 +46,12 @@ export function tokenizer(text: string): string[] {
   }
 
   return out;
+}
+
+function splitOnce(str: string, match: string): [string, string?] {
+  let i = str.indexOf(match);
+  if (i === -1) {
+    return [str, undefined]
+  }
+  return [str.slice(0, i), str.slice(i + match.length)]
 }
