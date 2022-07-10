4e6d76cb46e8af054f49eb21574807fd5390193f
Zach Hammer <zhammer@seatgeek.com>
2022-07-10 18:30:40 -0400

    try & fail to fix cypress

--- cypress/e2e/Directory.ts
+++ cypress/e2e/Directory.ts
@@ -1,34 +1,32 @@
-/* global cy */
-/// <reference types="cypress" />
-import { Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
-When(`I visit {string}`, (path) => {
+When(`I visit {string}`, (path: string) => {
   cy.visit(path);
 });
 
-When(`I click the link {string}`, (text) => {
+When(`I click the link {string}`, (text: string) => {
   cy.get("a").contains(text).click();
 });
 
-When(`I click the link {string} and refresh`, (text) => {
+When(`I click the link {string} and refresh`, (text: string) => {
   // manually reloading after clicks because for some reason
   // when cypress clicks a link, sometimes the url location changes
   // but the page content doesn't change.
   cy.get("a").contains(text).click().reload();
 });
 
-Then(`I am on {string}`, (path) => {
+Then(`I am on {string}`, (path: string) => {
   cy.location("pathname").should("eq", path);
 });
 
-Then(`I see {string}`, (text) => {
+Then(`I see {string}`, (text: string) => {
   cy.contains(text);
 });
 
-Then(`I see the piece {string} before {string}`, (before, after) => {
+Then(`I see the piece {string} before {string}`, (before: string, after: string) => {
   cy.get("table").children().contains(before).parents('tr').invoke('index').then(beforeIndex => {
     cy.get("table").children().contains(after).parents('tr').invoke('index').then(afterIndex => {
-      expect(afterIndex).greaterThan(beforeIndex);  
+      expect(afterIndex).greaterThan(beforeIndex);
     })
   })
 });
--- cypress/plugins/index.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-import cucumber from "cypress-cucumber-preprocessor";
-
-/**
- * @type {Cypress.PluginConfig}
- */
-export default (on, config) => {
-    on("file:preprocessor", cucumber.default());
-};
